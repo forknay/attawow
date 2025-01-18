c3d290e44a7009b780376ea0ee5459a0a321daf1
Jennifer You <jen.g.s.you@gmail.com>
2025-01-18 15:30:03 -0500

    LEMME GET NOODLES ADD CARD BUTTON
    add cards at a certain index
    requires variable for index + card info

--- hitster/src/Pack.jsx
+++ hitster/src/Pack.jsx
@@ -1,30 +1,27 @@
 import {useState} from "react";
 import Card from "./Card";
 
-const P1 = [{title: "Echoes in Rain", year: "1928", artist: "Lyra"},
+var P1 = [{title: "Echoes in Rain", year: "1928", artist: "Lyra"},
             {title: "Velvet Horizons", year: "1932", artist: "Mira"},
-            {title: "Fragments of Tomorrow", year: "1936", artist: "Aria"},
+            
             {title: "Neon Reverie", year: "1940", artist: "Nova"},
             {title: "Whispers of the Wild", year: "1944", artist: "Stella"}];
 
 const P2 = [{title: "Echoes in Rain", year: "1928", artist: "Lyra"}]
-//,
-//            {title: "Velvet Horizons", year: "1932", artist: "Mira"},
-//            {title: "Fragments of Tomorrow", year: "1936", artist: "Aria"},
-//            {title: "Neon Reverie", year: "1940", artist: "Nova"},
-//            {title: "Whispers of the Wild", year: "1944", artist: "Stella"}];
 
 
 
 function Pack(){
     const [pack, setPack] = useState(P1);
 
-    function removeCard(){
-        setPack(P2);
+    function addCard(){
+        let newPack = [...pack];
+        newPack.splice(2,0, {title: "Fragments of Tomorrow", year: "1936", artist: "Aria"})
+        setPack(newPack);
     }
     return(
         <>
-        <button onClick = {removeCard}>Remove</button>
+        <button onClick = {addCard}>add card</button>
         {pack.map((item) => <Card year = {item.year} title = {item.title} artist = {item.artist}/>)}
         
         </>
