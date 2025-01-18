8d3efbc7e974a6df4a934a079e6cdb6a22264298
Jessie Maeda-Yang <maeda.yang.jessie@gmail.com>
2025-01-17 22:22:41 -0500

    RPS Game Test

--- network.py
+++ network.py
@@ -1,4 +1,6 @@
 import socket
+import pickle
+
 
 class Network:
     def __init__(self):
@@ -6,10 +8,10 @@ class Network:
         self.server = "10.196.25.192"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
+        self.p = self.connect()
 
-    def getPos(self):
-        return self.pos
+    def getP(self):
+        return self.p
 
     def connect(self):
         try:
@@ -21,6 +23,6 @@ class Network:
     def send(self, data):
         try:
             self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048*2))
         except socket.error as e:
             print(e)
