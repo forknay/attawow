3fd8423735bbde80097900447f527c35e99908a5
Jessie Maeda-Yang <maeda.yang.jessie@gmail.com>
2025-01-19 07:37:21 -0500

    good game,card,player

--- clutchserv/nodejs/game.js
+++ clutchserv/nodejs/game.js
@@ -3,49 +3,55 @@ export default class Game {
         this.player_list = player_list;
         this.card_list = card_list;
         this.gameState = true;
-        this.playing = 0;
     }
 
     gameStart() {
         if (this.gameState == true) {
-            for (let player of this.player_list) {
+            for (let [id, player] of this.player_list) {
                 let start_card = getRandomCard(this.card_list)
-                player.addCard(start_card);
+                player.addCard(0, start_card);
+                console.log(player)
             }
             this.gameTurn();
         }
     }
 
-    gameTurn() {
+    gameTurn(guess) {
         if (this.gameState == true) {
             let turn_card = getRandomCard(this.card_list);
             turn_card.playSong();
-            
+
+            var correct = findIndex(turn_card, Array.from(this.player_list.values())[this.playing].getDeck());
+
             var counter = 30;
             const interval = setInterval(() => {
                 counter--;
 
                 if (counter <= 15 && counter >= 0) {
-                    console.log(counter)
+                    
+                }
+
+                if (correct.includes(guess)) {
+                    Array.from(this.player_list.values())[this.playing].addCard(guess, turn_card);
+                } else {
+                    this.turnEnd();
                 }
 
                 if (counter < 0 ) {
                     clearInterval(interval);
                     this.turnEnd();
                 }
-                //CHECK FOR IO FOR PLAYER ACTION
-                // if correct --> add card, else --> turn end
             }, 1000);
         }
     }
     
     turnEnd() {
         if (this.gameState == true) {
-            if (this.player_list[this.playing].getCount() == 5) {
+            if (Array.from(this.player_list.values())[this.playing].getCount() == 5) {
                 this.gameEnd();
             } else {
                 this.playing++;
-                this.playing = this.playing % this.player_list.length;
+                this.playing = this.playing % this.player_list.size;
 
                 var delay = 3;
                 const pause = setInterval(() => {
@@ -62,7 +68,7 @@ export default class Game {
 
     gameEnd() {
         this.gameState = false;
-        console.log("Game Over!\n" + this.player_list[this.playing].getID() + " won!\n");
+        console.log("Game Over!\n" + Array.from(this.player_list.ID())[this.playing] + " won!\n");
     }
 }
 
@@ -74,10 +80,15 @@ function getRandomCard(card_list) {
 }
 
 function findIndex(card, card_list) {
+    var equal = [];
     for (var i = 0; i < card_list.length; i++) {
+        if (card.getYear() == card_list[i].getYear()) {
+            equal.push(i);
+        }
         if (card.getYear() < card_list[i].getYear()) {
-            return i;
+            equal.push[i];
+            return equal;
         }
     }
-    return i;
+    return equal;
 }
