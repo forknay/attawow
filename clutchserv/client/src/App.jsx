74e57e217277faef9cdc3e9aaf26b0c969a4761e
julien <julienyang12@gmail.com>
2025-01-19 03:26:50 -0500

    socketio stuff

--- clutchserv/client/src/App.jsx
+++ clutchserv/client/src/App.jsx
@@ -1,17 +1,15 @@
 import './App.css';
 import Pack from '../../../src/Pack';
 import React from 'react';
-import Table from '../../../src/Table';
+
 
 function App() {
   return (
-    <>
+    
       <Pack></Pack>
-      <Table></Table>
-     
-     
       
-    </>
+    
+
   );
 }
 
--- clutchserv/nodejs/server.js
+++ clutchserv/nodejs/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/src')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client', 'index.html'));
 });
 
 const server = app.listen(PORT, () => {
-  console.log("Server is running on port 3001")
+  console.log("Server is running on port 3000")
 });
 
 var socket = require('socket.io');
@@ -24,5 +24,5 @@ var io = socket(server);
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
-  console.log(socket);
+  console.log('Server connection: ' + socket.id);
 }
